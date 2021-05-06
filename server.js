96997d5ff2994b620de8798521c48c01c23b18e9
darshilagrawal <darshilagrawal2000@gmail.com>
2021-05-06 23:26:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ app.get("/",function(req,res){
 })
 
 app.get("/contact",function(req,res){
-  res.send("Contact me: darshilagrawal2000@gmail.com");
+  res.send("Contact me: darshildevelops@gmail.com");
 })
 
 app.get("/pyaar",function(req,res){
-  res.send("Pyaar To hai CHAI");
+  res.send("Pyaar To thi CHAI");
 })
 app.get("/about",function(req,res){
   res.send("My Name is Darshil Agrawal, a 3rd Year Engineering Student trying to path my way to success and glory");
